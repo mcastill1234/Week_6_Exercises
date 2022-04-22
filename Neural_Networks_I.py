d665008bdc7d1a93c4348502a56de96296a7127b
mcastill <marcas2006@gmail.com>
2022-04-22 17:02:21 -0400

    Completed exercise 1A

--- Neural_Networks_I.py
+++ Neural_Networks_I.py
@@ -1,19 +1,15 @@
 import numpy as np
 
-# Exercise
-X = np.array([[0, 1, 2],
-              [0, 1, 2]])
+# Exercise 1A:
+X = np.array([[0, 1, 2], [0, 1, 2]])
 Y = np.array([[0, 1, 0]])
+W = np.array([[1, -1], [0, 0]])
+W0 = np.array([[-0.5], [1.5]])
 
-W = np.array([[1, -1],
-              [0, 0]])
+Z = W.T @ X + W0
+FZ = np.heaviside(Z, 1)
 
-W0 = np.array([[-0.5],
-              [1.5]])
-
-
-print(W.T @ X)
-print(W.T @ X + W0)
+print("First layer outputs", FZ.tolist())
 
 
 
